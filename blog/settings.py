6f6ecd0040e6291f1ae0b10864f73a6ea1b91022
Bumper0417 <chrisgoros13@gmail.com>
2020-02-26 18:25:19 +0000

    Fixed mistakes in spelling

--- blog/settings.py
+++ blog/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoine.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
