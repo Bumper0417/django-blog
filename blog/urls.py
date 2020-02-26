39b27043c458892847b36b7faaaeeb8c62500b21
Bumper0417 <chrisgoros13@gmail.com>
2020-02-26 16:37:51 +0000

    Added Heroku and Postgres support for deployment

--- blog/urls.py
+++ blog/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views import RedirectView
+from django.views.generic import RedirectView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',RedirectView.as_view(url='posts/')),
+    url(r'^$', RedirectView.as_view(url='posts/')),
     url(r'posts/', include('posts.urls')),
-    url(r'^media/(?P<path>.*$', serve, {'document_root': MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 ]
